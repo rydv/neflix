5426185f4c71de8ea17269068963d25650b58445
Sanjay <sanjayrydv@gmail.com>
2023-05-20 01:44:03 +0530

    models created

---
+++ api/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required:true, unique: true},
+    email:{ype: String, required:true, unique: true},
+    password:{ype: String, required:true},
+    profilePic: {type:String, default:""},
+    isAdmin: {type:Boolean, default: false}
+},
+{timestamps:true}
+);
+
+module.exports = mongoose.model("User", UserSchema);
