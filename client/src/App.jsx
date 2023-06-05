7fb219656a5845679885f373d4778fd10f90798e
Sanjay <sanjayrydv@gmail.com>
2023-06-05 14:04:06 +0530

    routing in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,23 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
+    <Router>
+        <Routes>
+          <Route path="/" Component={Home}/>
+          <Route path="/movies" Component={Home}/>
+          <Route path="/series" Component={Home}/>
+          <Route path="/register" Component={Register }/>
+          <Route path="/login" Component={Login}/>
+          <Route path="/watch" Component={Watch}/>
+        </Routes>
+    </Router>
   // <Home/>
   // <Watch />
-  <Register />
+  // <Register />
   // <Login />
   )
 };
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,10 +3,11 @@ import List from "../../components/list/List"
 import Navbar from "../../components/navbar/Navbar"
 import "./home.scss"
 const Home = () => {
+  const type = window.location.pathname.split('/')[1]
   return (
     <div className="home">
         <Navbar/>
-        <Featured type="movie"/>
+        <Featured type={type}/>
         <List />
         <List />
         <List />
